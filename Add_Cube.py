940f46acd7fe1a49e23d17206b03a61c404f548c
PolyU-SnowKing <gyk16@tsinghua.org.cn>
2024-11-07 15:22:48 +0800

    Add_Samples
    add some official sample of coding

--- Add_Cube.py
+++ Add_Cube.py
@@ -3,20 +3,18 @@ bl_info = {
     "author": "24062576g",
     "version": (1, 0),
     "blender": (2, 80, 0),
-    "location": "View3D > Add > Mesh > New Object",
+    "location": "View3D > Sidebar > NPRtoolkit",
     "description": "Adds a new Mesh Object",
     "warning": "",
     "doc_url": "",
     "category": "Add Mesh",
 }
 
-
 import bpy
-from bpy.types import Operator
+from bpy.types import Operator, Panel
 from bpy.props import FloatVectorProperty
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from mathutils import Vector
-
 
 def add_object(self, context):
     scale_x = self.scale.x
@@ -34,10 +32,7 @@ def add_object(self, context):
 
     mesh = bpy.data.meshes.new(name="New Object Mesh")
     mesh.from_pydata(verts, edges, faces)
-    # useful for development when the mesh may be invalid.
-    # mesh.validate(verbose=True)
     object_data_add(context, mesh, operator=self)
-
 
 class OBJECT_OT_add_object(Operator, AddObjectHelper):
     """Create a new Mesh Object"""
@@ -53,41 +48,27 @@ class OBJECT_OT_add_object(Operator, AddObjectHelper):
     )
 
     def execute(self, context):
-
         add_object(self, context)
-
         return {'FINISHED'}
 
+class VIEW3D_PT_NPRtoolkit(Panel):
+    bl_label = "NPRtoolkit"
+    bl_idname = "VIEW3D_PT_NPRtoolkit"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'NPRtoolkit'
 
-# Registration
-
-def add_object_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_add_object.bl_idname,
-        text="Add Object",
-        icon='PLUGIN')
-
-
-# This allows you to right click on a button and link to documentation
-def add_object_manual_map():
-    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
-    url_manual_mapping = (
-        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
-    )
-    return url_manual_prefix, url_manual_mapping
-
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("mesh.add_object")
 
 def register():
     bpy.utils.register_class(OBJECT_OT_add_object)
-    bpy.utils.register_manual_map(add_object_manual_map)
-    bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
-
+    bpy.utils.register_class(VIEW3D_PT_NPRtoolkit)
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_add_object)
-    bpy.utils.unregister_manual_map(add_object_manual_map)
-    bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
-
+    bpy.utils.unregister_class(VIEW3D_PT_NPRtoolkit)
 
 if __name__ == "__main__":
     register()
